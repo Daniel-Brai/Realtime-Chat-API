9f691f9ccaa46449d4c57a40010feb3ea75881a1
Daniel Brai <88239970+Daniel-Brai@users.noreply.github.com>
2022-06-10 13:57:18 +0100

    Added main route for heroku deployment

--- server.js
+++ server.js
@@ -23,7 +23,15 @@ connectDB()
 app.use(express.json());
 app.use(cors());
 
-// register the routes
+// main route
+app.get('/', (req, res) => {
+  res.status(200).send(
+    `<h2 style='display: flex; align-items: center; justify-content: center; color: #ADD8E6'>Welcome to my API 🙂</h2>`
+  )
+ }
+)
+
+// register, login and invitation routes
 app.use("/api/auth", authRoutes);
 app.use("/api/friend-invitation", friendInvitationRoutes);
 
@@ -31,7 +39,7 @@ app.use("/api/friend-invitation", friendInvitationRoutes);
 const server = http.createServer(app);
 socketServer.registerSocketServer(server);
 
-const PORT = process.env.PORT || process.env.API_PORT;
+const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
   console.log(`⚡ [server]: Server is listening on ${PORT}`);
 });
